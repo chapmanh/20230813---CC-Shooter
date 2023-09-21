f8ff1e850e40b1480d09bb0ddc4660279ea47b90
Haydn <haydn.chapman@hotmail.com>
2023-09-22 00:26:06 +0100

    Grenade Animation

--- scenes/Projectiles/grenade.gd
+++ scenes/Projectiles/grenade.gd
@@ -1,3 +1,12 @@
 extends RigidBody2D
 
 const speed = 750
+
+func explode():
+	$CollisionShape2D.disabled = true
+	$Sprite2D.visible = false
+	print("Starting Explosion")
+	$AnimationPlayer.play("explosion")
+	
+func free():
+	self.call_deferred("queue_free")
