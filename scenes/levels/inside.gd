88ce5c3686a61685b94f0696a60ffaa401d40a85
Haydn <haydn.chapman@hotmail.com>
2023-10-01 19:29:45 +0100

    Level transition fix
    using @export var level_scene: PackedScene seems to be having issues with uniqueness. Godot is adding on extra numbers to the end of the file name and then not finding the scene after it transitions successfully the first time. Sticking with hard coded scene paths in the load_scene_from_file() function for now.

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,8 +1,6 @@
 extends BaseLevel
 
-@export var outside_level_scene: PackedScene
-
 func _on_entrance_body_entered(_body):
 	var tween = create_tween()
 	tween.tween_property(%Player, "speed", 0, 0.5)
-	get_tree().change_scene_to_packed(outside_level_scene)
+	get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -1,11 +1,9 @@
 extends BaseLevel
 
-@export var inside_level_scene: PackedScene
-
 func _on_gate_entered(_body):
 	var tween = create_tween()
 	tween.tween_property(%Player, "speed", 0, 0.5)
-	get_tree().change_scene_to_packed(inside_level_scene)
+	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
 
 func _on_house_player_entered(_body):
 	adjust_zoom(ZOOM_INDOORS)
