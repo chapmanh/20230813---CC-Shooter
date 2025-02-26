c9db8a2cf25d7d933fe4795cd73e21877517f7a1
Haydn <haydn.chapman@hotmail.com>
2025-02-26 23:06:13 +0000

    Grenade area hit

--- scenes/Projectiles/grenade.gd
+++ scenes/Projectiles/grenade.gd
@@ -7,11 +7,16 @@ func _ready():
 	angular_velocity = randf_range(-1, 1) * 2 * PI
 	$Node/Explosion.rotation = randf_range(-PI, PI)
 	
-
 func explode():
 	$CollisionShape2D.disabled = true
 	$RemoteTransform2D.update_position = false
 	$AnimationPlayer.play("explosion")
+	
+func damage():
+	for body in $Node/Area2D.get_overlapping_bodies():
+		if "hit" in body:
+			print(body)
+			body.hit()
 	
 func free():
 	self.call_deferred("queue_free")
